dc8751e577045a8ca4d317b4de51e03c854406c6
Lívia Rocha <livialdarocha@gmail.com>
2025-08-27 09:25:52 -0300

    calculadora

--- nodejs/modulos/app.js
+++ nodejs/modulos/app.js
@@ -1,4 +1,7 @@
-const saudacao = require('./meuModulo'); // Importando o módulo
+const modulo = require('./meuModulo'); // Importando o módulo
+const calculadora_idade = require('./calculadora_idade'); 
+const idadeEm2025 = calculadora_idade.idade('Roberval',1990); 
+const mensagem = modulo.saudacao('Joédio'); // Executando a função
 
-const mensagem = saudacao('Joédio'); // Executando a função
 console.log(mensagem);
+console.log(idadeEm2025);
---
+++ nodejs/modulos/calculadora_idade.js
@@ -0,0 +1,9 @@
+function idade(nome,anoNasc) {
+    idade = 2025 - anoNasc;
+    return `Olá, ${nome}! Em 2025 você terá ${idade} anos!`;
+}
+
+// Exportando a função para que possa ser usada em outros arquivos
+module.exports = {
+    idade
+};
