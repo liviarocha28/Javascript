74e820402ef67e4f7561e6dd5a8f758b75565477
Lívia Rocha <livialdarocha@gmail.com>
2025-09-05 09:25:30 -0300

    Versão http

--- nodejs/modulos/http/app.js
+++ nodejs/modulos/http/app.js
@@ -6,6 +6,7 @@ http.createServer((req, res) => {
     //definindo o cabeçalho da resposta com status 200 (OK) e tipo de conteúdo text/plain
     res.writeHead(200, {'Content-Type': 'text/plain'});
     //enviando a resposta "Hello World!"
-    res.end('Hello World!');
+    res.end('Ola, Mundo!');
     //adicionando um log no console para cada requisição recebida
 }).listen(8080);
+console.log('Servidor rodando em http://localhost:8080/');
---
+++ nodejs/modulos/http/cliente.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+http.get('http://jsonplaceholder.typicode.com/posts/1', (res) => {
+    let data = '';
+
+    //Um pedaço de dado foi recebido.
+    res.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    //A resposta inteira foi recebida. Imprime o resultado.
+    res.on('end', () => {
+        console.log(JSON.parse(data));
+    });
+
+}).on('error', (err) => {
+    console.log("Erro: " + err.message);
+});
